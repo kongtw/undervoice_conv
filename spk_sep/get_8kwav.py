398b3f1b98b38c74ef426a9fe8d78b09b4a127d1
wangkongtao <wangkongtao@139.com>
2018-07-23 23:34:04 +0800

    'v1.0'

--- spk_sep/get_8kwav.py
+++
@@ -1,8 +0,0 @@
-import commands
-import glob
-
-for wav_name in glob.glob('./data/pcm/spkregister/*'):
-    dest_name = './data/pcm/spkregister_8k/' + wav_name.split('/')[-1]
-    print dest_name
-    sox_cmd = 'sox -r 6k ' + wav_name + ' -r 8k ' + dest_name
-    (status, output) = commands.getstatusoutput(sox_cmd)
--- spk_sep/test_command.py
+++
@@ -1,6 +0,0 @@
-import os
-
-cmd_str = 'bin/IvExtractor --config ./cfg_rt/ivExtractor_fast_25520.cfg --saveVectorFilesPath /home/dell/runtimedata/spk_test_data/iv_raw/ ' \
-          '--featureFilesPath /home/dell/runtimedata/spk_test_data/prm/ --labelFilesPath /home/dell/runtimedata/spk_test_data/lbl/ --targetIdList /home/dell/runtimedata/spk_test_data/ndx/25520_ivExtractor.ndx'
-
-os.system(cmd_str)
